b4929e001259ac44a127686e15111b2a8f6b01a3
John Coleman <jbcolemanjr@gmail.com>
2023-08-08 16:14:05 -0400

    complete dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+const excise = document.querySelector('main#main')
+excise
+const newHeader = document.createElement('h1')
+//newHeader
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = "John is the champion"
+document.body.append(newHeader)
